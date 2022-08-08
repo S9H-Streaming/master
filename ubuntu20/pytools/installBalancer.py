1fcb27c98e549a724f31af0aa1bf2d19cbfb728b
S9H-Streaming <110831689+S9H-Streaming@users.noreply.github.com>
2022-08-08 17:50:33 +0200

    Update installBalancer.py

--- ubuntu20/pytools/installBalancer.py
+++ ubuntu20/pytools/installBalancer.py
@@ -4,7 +4,7 @@ import subprocess, os, sys
 from itertools import cycle, izip
 
 rDownloadURL = "http://tekosafe.net/xteko3/sub_xtreamcodes_reborn.tar.gz"
-rPackages = ["libcurl4", "libxslt1-dev", "libgeoip-dev", "e2fsprogs", "wget", "mcrypt", "nscd", "htop", "zip", "unzip", "mc", "libzip5"]
+rPackages = ["libcurl4", "libxslt1-dev", "libgeoip-dev", "e2fsprogs", "wget", "mcrypt", "nscd", "htop", "zip", "unzip", "mc", "libpng16-16", "libzip5", "python-is-python2", "libonig-dev" , "mariadb-server"]
 
 def getVersion():
     try: return subprocess.check_output("lsb_release -d".split()).split(":")[-1].strip()
